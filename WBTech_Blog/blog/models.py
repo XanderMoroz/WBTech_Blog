9a24be3d71ded676024114ec7ada463327b9b0fc
Александр <7176uiop3436>
2022-09-21 01:17:10 +0300

    Правка моделей

--- WBTech_Blog/blog/models.py
+++ WBTech_Blog/blog/models.py
@@ -8,6 +8,9 @@ class Author(models.Model):
     postCount = models.IntegerField(default=0)
     lovelyAuthors = models.ManyToManyField(User, through='UserLovelyAuthors', related_name='user_Author')
 
+    def __str__(self):
+        return f'{self.user}'
+
 class Post(models.Model):
     '''
     Эта модель описывает пост, которые создают пользователи.
@@ -18,12 +21,18 @@ class Post(models.Model):
     - заголовок объявления;
     - текст объявления.
     '''
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор объявления')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Автор объявления')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     content = models.CharField(max_length=256, verbose_name='Содержание')
     status = models.CharField(max_length=6, choices=[('UNREAD', 'Непрочитанный'), ('READ', 'Прочитанный')], default='UNREAD',
                                 verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.title} {self.author}'
+
+    def get_absolute_url(self):  # добавим абсолютный путь, чтобы после создания нас перебрасывало на страницу с товаром
+        return f'/posts/{self.id}'
 
 class UserLovelyAuthors(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
