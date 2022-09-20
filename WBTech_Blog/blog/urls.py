3881a28401546e6c1849b62563c4177426f6d66f
Александр <7176uiop3436>
2022-09-20 21:21:27 +0300

    Созданы урлы: 'posts/' и 'posts/<int:pk>'

--- WBTech_Blog/blog/urls.py
+++ WBTech_Blog/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import AuthorList, AuthorDetail
-
+from .views import *
 
 urlpatterns = [
     path('authors/', AuthorList.as_view(), name='author_list'),
     path('authors/<int:pk>', AuthorDetail.as_view(), name='author_detail'),
 
-
+    path('posts/', PostList.as_view(), name='post_list'),
+    path('posts/<int:pk>', PostDetail.as_view(), name='post_detail'),
     ]
