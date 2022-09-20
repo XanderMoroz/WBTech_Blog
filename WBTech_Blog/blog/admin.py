cbda4767ba1bbb07f349004668203d5bc78bbb46
Александр <7176uiop3436>
2022-09-20 20:53:40 +0300

    Регистрация моделей в админке

--- WBTech_Blog/blog/admin.py
+++ WBTech_Blog/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Author, UserLovelyAuthors
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(UserLovelyAuthors)
