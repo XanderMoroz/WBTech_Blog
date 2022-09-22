e9bf828dcd5f5a6c8d71b98ab91efa3a79c6dcc0
Александр <7176uiop3436>
2022-09-22 17:57:19 +0300

    Регистрация модели: UserReadPost

--- WBTech_Blog/blog/admin.py
+++ WBTech_Blog/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Author, UserSubscribers
+from .models import Post, Author, UserSubscribers, UserReadPost
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(UserSubscribers)
+admin.site.register(UserReadPost)
